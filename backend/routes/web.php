980a567488c3e49a0eb6f3ad2f581d054fe9d42e
Himanshu Sangwan <sangwan.himanshu98@gmail.com>
2025-06-30 13:56:57 +0530

    undo testing changes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
+use App\Helpers\ResponseHelper;
 use Illuminate\Support\Facades\Route;
 
 Route::get(
     '/',
     function () {
-        (new \App\Services\EmailService)->addTo('test')->addBcc('tesadfasdoif')->setSubject('Hello This is refactored Email Service')->sendEmailWithTemplate(
-            'test',
-            [
-                'name' => 'Himanshu Sangwan',
-                'message' => 'This is a test email from EmailService.',
-            ],
-        );
+        return ResponseHelper::successResponse('Welcome to the API!');
     },
 );
